13b860fa3e82963cdd2728a40b66480eedd315f2
Michael <eugenemichael1999@gmail.com>
2020-09-10 23:12:50 +0100

    added images to news articles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="home">
     <button @click="getNews" class="bg-red-400 rounded border border-black">Display News</button>
-      <p class="text-xl">Top Headlines</p>
-    <div v-for="(article, index) in topHeadlines" :key="index" v-show="isLoaded">
-
+    <p class="text-xl">Top Headlines</p>
+    <div class="flex flex-wrap justify-center items-center">
       <!-- BEGIN -->
 
-      <div class="max-w-sm w-full lg:max-w-full lg:flex">
+      <div
+        class="w-full max-w-sm m-4"
+        v-for="(article, index) in topHeadlines"
+        :key="index"
+        v-show="isLoaded"
+      >
         <div
-          class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-          style="`background-image: url('${article.urlToImage}')`"
+          class="h-48 flex-none bg-cover rounded-b-none rounded text-center overflow-hidden"
+          :style="`background-image: url('${article.urlToImage}')`"
           title="Woman holding a mug"
         ></div>
         <div
-          class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal"
+          class="border-r border-b border-l border-gray-400 bg-white rounded-b lg:rounded-t-none rounded p-4 flex flex-col justify-between leading-normal"
         >
           <div class="mb-8">
-            <div
-              class="text-gray-900 font-bold text-xl mb-2"
-            >{{article.title}}</div>
-            <p
-              class="text-gray-700 text-base"
-            >{{article.description}}</p>
+            <div class="text-gray-900 font-bold text-xl mb-2">{{article.title}}</div>
+            <p class="text-gray-700 text-base">{{article.description}}</p>
           </div>
           <div class="flex items-center">
             <div class="text-sm">
@@ -59,7 +59,7 @@ export default {
         .then((data) => {
           console.log(data.articles);
           this.topHeadlines = data.articles;
-          this.isLoaded = true
+          this.isLoaded = true;
         });
     },
   },
